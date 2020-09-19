60f0e295c63f87ac7f846576ad402babe1515189
Rajarshi <58479147+rajarshisur@users.noreply.github.com>
2020-09-20 00:13:27 +0530

    Create Exercise1_3.java
    Consider first n even numbers starting from zero(0). Complete the code segment to calculate sum of  all these numbers divisible by 3. Print the sum.
    
    Example:
    
    Input: n = 5
    
    -------
    0 2 4 6 8
    Even number divisible by 3:0 6
    sum:6

---
+++ Week 1/Exercise1_3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exercise1_3 {
+       public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n=sc.nextInt();
+	   int sum=0;
+//Use for or while loop do the operation
+	   int t=0;
+	   for(int i=0;i<=n;i++)
+		{
+			t = 2*(i-1);
+			if(t%3 == 0)
+			{
+				sum += t;
+			}
+		}
+		System.out.println(sum);
+  }
+}    
