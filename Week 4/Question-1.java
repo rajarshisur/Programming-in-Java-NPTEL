a71b731f3feb5440eb2342c33f4bfe9d1e5ae9b5
Rajarshi <58479147+rajarshisur@users.noreply.github.com>
2020-11-02 10:37:26 +0530

    Create Question-1.java
    Complete the code segment to execute the following program successfully. You should import the correct package(s) and/or class(s) to complete the code.

---
+++ Week 4/Question-1.java
@@ -0,0 +1,16 @@
+/*Complete the code segment to execute the following program successfully. You should import the correct package(s) and/or class(s) to complete the code.*/
+
+// Import the required package(s) and/or class(es)
+import static java.lang.System.out;
+import java.util.Scanner;
+// main class Question is created
+public class Question41{
+  public static void main(String[] args) {
+	// Scanner object is created
+    Scanner scanner = new Scanner(System.in);
+     // Read String input using scanner class
+    String courseName = scanner.nextLine(); 
+     // Print the scanned String
+    out.println("Course: " + courseName); 
+  }
+}
